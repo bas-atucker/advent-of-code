9791b83a6eedc424079dac25fd7e22062ed04bc7
Adam Tucker <adam.tucker@bigasssolutions.com>
2016-12-05 15:43:26 -0500

    Day 5 complete

--- aoc-5-2/src/main.rs
+++ aoc-5-2/src/main.rs
@@ -1,26 +1,31 @@
 #![feature(slice_patterns)]
 
 extern crate md5;
+use std::collections::BTreeMap;
 
 const INPUT: &'static str = "wtnhxymk";
 
 fn main() {
-    let mut output: String = String::from("");
     let input: String = String::from(INPUT);
-
+    let mut result: BTreeMap<u8, String> = BTreeMap::new();
     let mut i: i32 = 0;
     
-    while output.len() < 8 {
+    while result.len() < 8 {
         let curr = format!("{}{}", &input, i);
         let digest = md5::compute(curr.as_bytes());
 
         if digest[0] == 0 && digest[1] == 0 && (digest[2] & 0xF0)==0 {
-            output += &format!("{0:x}", digest[2] & 0x0F);
-            println!("{}", i);
+            let pos = digest[2] & 0x0F;
+
+            if pos < 8 && !result.contains_key(&pos) {
+                result.insert(pos, format!("{0:x}", digest[3] >> 4));
+            }
         };
 
         i += 1;
     }
 
-    println!("{}", output);
+    for (_, val) in result {
+        print!("{}", val);
+    }
 }
